e0f7d27c9f4ffbfabcda7656e92565d38aaf157d
Krishna <krishna@Krishnas-MacBook-Air.local>
2026-09-25 20:38:59 +0530

    MYSMME updated

--- sku/urls.py
+++ sku/urls.py
@@ -23,6 +23,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("whatsapp/", include("whatsapp_crm.urls"),),
     path("accounts/", include("accounts.urls")),
     path("", include("main.urls")),
 ]
